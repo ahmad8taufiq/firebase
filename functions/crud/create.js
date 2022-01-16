592d3c601846d5126cd281e28eca9904a639c658
Ahmad Taufiq <ahmadtaufiq879@gmail.com>
2022-01-16 16:05:22 +0700

    [ADD] Other way to add

--- functions/crud/create.js
+++ functions/crud/create.js
@@ -1,12 +1,25 @@
 const db = require('../config')
 
-const docRef = db.collection('users').doc('KcazbrjWYOMZuf9OnkAR');
+// const docRef = db.collection('users').doc('KcazbrjWYOMZuf9OnkAR');
 
-docRef.set({
-  firstName: 'Ahmad',
-  lastName: 'Taufiq',
-}).then(result => {
-  console.log(result)
-}).catch(err => {
-  console.log(err)
+// Using set
+// docRef.set({
+//   firstName: 'Ahmad',
+//   lastName: 'Taufiq',
+// }).then(result => {
+//   console.log(result)
+// }).catch(err => {
+//   console.log(err)
+// })
+
+// Using add
+db.collection("users").add({
+  firstName: "Ahmad",
+  lastName: "Taufiq"
 })
+.then((docRef) => {
+  console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+  console.error("Error adding document: ", error);
+});
